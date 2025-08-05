f9661ffcf3ab8cb951305ff2e7a1d4f52d2cf93c
Marek Lachman <mareklachman@centrum.cz>
2025-08-05 15:41:11 +0200

    Converted to Minimal API

--- src/SeaBattle.API/Program.cs
+++ src/SeaBattle.API/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Scalar.AspNetCore;
+using SeaBattle.API.ViewModels;
 using SeaBattle.Application;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +19,21 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapPost("api/games", Ok<GameViewModel> (CreateGameModel model, World world) =>
+{
+    Game game = world.CreateGame(model.FirstPlayerName, model.SecondPlayerName, model.MapSize);
+    return TypedResults.Ok(new GameViewModel(game.Id, game.State.ToString(), game.CurrentPlayerName));
+});
+
+app.MapPost("api/games/{id:guid}/moves", Results<Ok<FireResultModel>, NotFound> (Guid id, FireModel model, World world) =>
+{
+    Game? game = world.GetGame(id);
+    if (game == null) return TypedResults.NotFound();
+
+    FireResult result = game.Fire(new Position(model.X, model.Y));
+    return TypedResults.Ok(new FireResultModel(result.ToString(), game.State.ToString(), game.CurrentPlayerName));
+});
 
 app.MapControllers();
 
