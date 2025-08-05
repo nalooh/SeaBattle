c7a805bb4e52253adb757365423986400305a423
Marek Lachman <mareklachman@centrum.cz>
2025-08-05 16:09:29 +0200

    Comments

--- src/SeaBattle.Application/FireResult.cs
+++ src/SeaBattle.Application/FireResult.cs
@@ -1,10 +1,15 @@
 namespace SeaBattle.Application;
 
+/// <summary>Výsledek palby</summary>
 public enum FireResult
 {
+    /// <summary>Vedle</summary>
     Miss,
+    /// <summary>Zásah</summary>
     Hit,
+    /// <summary>Potopená loď</summary>
     Sunk,
+    /// <summary>Výhra</summary>
     Win,
 
 }
